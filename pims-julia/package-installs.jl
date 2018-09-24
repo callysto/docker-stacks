2b4de2c1a7e1d07e7640e85323bfe56dd34643da
Jesse Perla <jesseperla@gmail.com>
2018-09-24 13:39:22 -0700

    Removed pacakges incompatible with v1.0
    IPOPT is compatible, but the image would need a C compiler

--- pims-julia/package-installs.jl
+++ pims-julia/package-installs.jl
@@ -1,3 +1,5 @@
 import Pkg
 
-Pkg.pkg"add BinDeps Cairo Calculus Clustering Clp Colors DataArrays DataFrames DataFramesMeta Dates DecisionTree Distributions Distances GLM HDF5 HypothesisTests IJulia Ipopt JSON KernelDensity Lazy MLBase MultivariateStats NLopt Optim ODE PDMats PGFPlots Plots PyCall PyPlot QuantEcon RDatasets SQLite Stan StatsBase Sundials ZipFile ZMQ; precompile"
+Pkg.pkg"add BinDeps Calculus Clustering Clp Colors DataArrays DataFrames DataFramesMeta Dates DecisionTree Distributions Distances GLM HypothesisTests IJulia Ipopt JSON KernelDensity Lazy MLBase MultivariateStats NLopt Optim ODE PDMats PGFPlots Plots PyCall PyPlot QuantEcon RDatasets SQLite Stan StatsBase Sundials ZipFile ZMQ; precompile"
+
+#Not compatible yet: Cairo HDF5 IPOPT
