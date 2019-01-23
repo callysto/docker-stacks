ba940372fa0604661b25829f6d0af200ee7375ab
Callysto Sysadmin <sysadmin@callysto.ca>
2019-01-23 17:06:47 +0000

    Check for read-only home dir on container launch

--- base-notebook/start-notebook.sh
+++ base-notebook/start-notebook.sh
@@ -4,6 +4,10 @@
 
 set -e
 
+# Fail if the home directory is not writeable
+touch ~/writeable.txt
+rm ~/writeable.txt
+
 if [[ ! -z "${JUPYTERHUB_API_TOKEN}" ]]; then
   # launched by JupyterHub, use single-user entrypoint
   exec /usr/local/bin/start-singleuser.sh $*
